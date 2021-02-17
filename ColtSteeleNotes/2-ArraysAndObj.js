fda8044cecca74a8a29920686d2c8962f4667bbb
ajakcyer <ajakcyer97@gmail.com>
2021-02-16 23:18:34 -0500

    objectives to apply big o to something tangible

---
+++ ColtSteeleNotes/2-ArraysAndObj.js
@@ -0,0 +1,8 @@
+/* 
+            OBJECTIVES
+    
+    - Understand how objects and arrays work through the lens of Big O (is there a fast way? is there a slow way?)
+    - Explain why adding element to the beginning of an array is costly (is there a better way?)
+    - Compare and contrast the runtime for arrays and objects, as well as built-in methods
+
+*/
