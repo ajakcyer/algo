e7e114617254da51a4f9e20dea89544e2e6dbd6a
ajakcyer <ajakcyer97@gmail.com>
2021-02-17 21:03:40 -0500

    what is an algorithm

---
+++ ColtSteeleNotes/3-ProblemSolving.js
@@ -0,0 +1,19 @@
+//                                      Algorithms and Problem Solving Approaches
+//                                     -------------------------------------------
+
+/*
+            OBJECTIVES
+    - Define what an algorithm is
+    - Create a plan to solve algorithms
+    - Compare and contrast problem solving patterns
+*/
+
+
+/*
+            What is an Algorithm?
+    - A process or set of steps to accomplish a certain task
+    
+    Almost EVERYTHING you do in programming involves algorithms.
+    It's the foundation for being a successful problem solver and developer
+*/
+
