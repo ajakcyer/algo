be767e0a700f2dd00a865e0d39be01bfcc5fbec8
ajakcyer <ajakcyer97@gmail.com>
2021-02-22 14:54:00 -0500

    understanding the problem

--- ColtSteeleNotes/3-ProblemSolving.js
+++ ColtSteeleNotes/3-ProblemSolving.js
@@ -1,5 +1,5 @@
-//                                      Algorithms and Problem Solving Approaches
-//                                     -------------------------------------------
+//                         Algorithms and Problem Solving Approaches
+//                        -------------------------------------------
 
 /*
             OBJECTIVES
@@ -37,3 +37,25 @@
     - Look back and refactor
 
 */
+
+
+
+/*
+            UNDERSTAND THE PROBLEM
+            ----------------------
+        (questions to ask yourself)
+
+    1. Can I restate the problem in my own words? Not word-for-word.
+    2. What are the inputs that go into the problem?
+    3. What are the outputs, what should be returned?
+    4. Can the outputs be determined from the inputs?
+    5. How should I label the important pieces of data that are a part of the problem
+
+*/
+
+/*
+            PROBLEM SOLVING
+            ---------------
+
+*/
+
