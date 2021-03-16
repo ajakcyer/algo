51b7cd0bd2bc11025c68f6bec11b177acba2613f
ajakcyer <ajakcyer97@gmail.com>
2021-03-15 21:15:54 -0400

    refactoring questions

--- ColtSteeleNotes/3-ProblemSolving.js
+++ ColtSteeleNotes/3-ProblemSolving.js
@@ -152,3 +152,17 @@ function charCount(str){
 }
 
 console.log(charCount("Your PIN number is 1234!"))
+
+/*
+            Look Back & Refactor
+            --------------------
+            Refactoring Questions:
+    
+    - Can you check the result?
+    - Can you derive the result differently? (is there other ways to do this?)
+    - Can you understand it at a glance?
+    - Can you use the result or method for some other problem
+    - Can you improve the performance of your solution
+    - Can you think of other ways to refactor
+    - How have other people solved this problem?
+*/
