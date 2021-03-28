45a73872b0b088c9f5de4d8f882268a3fc2621fe
ajakcyer <ajakcyer97@gmail.com>
2021-03-28 18:09:59 -0400

    trying frequency counter example

--- ColtSteeleNotes/PSPatterns.js
+++ ColtSteeleNotes/PSPatterns.js
@@ -19,3 +19,27 @@
     - Backtracking
     - Many more...
 */
+
+
+/*
+        FREQUENCY COUNTERS
+
+    Uses an object or sets collect values/frequencies of values
+
+    This avoids the need for nested loops O(n^2) with arrays/strings
+    
+*/
+
+/*                      EXAMPLE 1
+    Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding values squared in the second array. The frequency of values must be the same.
+
+    same([1,2,3], [4,1,9]) returns true
+    same([1,2,3], [1,9]) returns false
+    same([1,2,1], [4,4,1]) returns false (must be same freq)
+*/
+
+function same(arrayOne, arrayTwo){
+
+
+    
+}
