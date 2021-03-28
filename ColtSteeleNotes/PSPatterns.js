81c56f714cf2060fca53754151d0d649769047ce
ajakcyer <ajakcyer97@gmail.com>
2021-03-28 17:58:56 -0400

    Blueprint for some algorithm patterns

---
+++ ColtSteeleNotes/PSPatterns.js
@@ -0,0 +1,21 @@
+//              Problem Solving Patterns
+//              -----------------------
+
+/*
+    1. DEVISE a plan for solving problesm
+    2. MASTER common problem solving patterns
+            -- for common approaches (DOES NOT COVER ALL SCENARIOS)
+*/
+
+/*
+        SOME PATTERNS...
+
+    - Frequency Counter
+    - Multiple Pointers
+    - Sliding Window
+    - Divide and Conquer
+    - Dynamic Programming
+    - Greedy Algorithms
+    - Backtracking
+    - Many more...
+*/
