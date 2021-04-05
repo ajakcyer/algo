76bc95b7c129d8ef9ddc87df52bae91f8d33a650
ajakcyer <ajakcyer97@gmail.com>
2021-04-05 16:37:08 -0400

    looping through each array to create an object that has the key of the array element and the value of how many times that element is found in that array

--- ColtSteeleNotes/PSPatterns.js
+++ ColtSteeleNotes/PSPatterns.js
@@ -73,7 +73,7 @@ function mySame(arrayOne, arrayTwo){
     // returns true or false
 }
 
-mySame([1,2,3], [4,4,1])
+// mySame([1,2,3], [4,4,1])
 
 /*          Colt's version          */
 function same(arr1, arr2){
@@ -84,6 +84,15 @@ function same(arr1, arr2){
     let frequencyCounter1 = {};
     let frequencyCounter2 = {};
 
-    
+    for (let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
 
+    for (let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+    console.log(frequencyCounter1)
+    console.log(frequencyCounter2)
 }
+
+same([1,2,3], [4,4,1])
