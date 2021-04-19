b8640a8087b886ff055974c8f7c39550569656cd
ajakcyer <ajakcyer97@gmail.com>
2021-04-19 18:55:13 -0400

    multiple pointers example

--- ColtSteeleNotes/4-PSPatterns.js
+++ ColtSteeleNotes/4-PSPatterns.js
@@ -172,3 +172,38 @@ function validAnagram(string1, string2){
 }
 
 console.log(validAnagram('anagram', 'nagaram'))
+
+
+// --------------------------------------
+
+/*
+        MULTIPLE POINTERS
+
+        Creating pointers or values that correspond to an index or position and move
+        towards the begining, end or middle based on a certain condition.
+
+        Great for solving problems with minimal space complexity too!
+
+        EX)
+
+        [-4,-3,-2,-1,0,1,2,5]
+        "asdashlfksafasfdfdkld"
+
+    
+*/
+
+/*
+
+EXAMPLE)
+
+Write a function called sumZero which accepts a sorted array (lowest to highest) of integers.
+The function should find the first pair where the sum is 0. Return an
+array that includes both values that sum to zero or undefined if a pair does
+not exist
+
+sumZero([-3,-2,-1,0,1,2,3]) -> [-3,3]
+sumZero([-2,0,1,3]) -> undefined
+sumZero([1,2,3]) -> undefined
+
+
+*/
