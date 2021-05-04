e50543f949c76619db0fb6e59ebbf9d60391464b
ajakcyer <ajakcyer97@gmail.com>
2021-05-03 23:45:10 -0400

    definition of Sliding Window pattern and what it is

--- ColtSteeleNotes/4-PSPatterns.js
+++ ColtSteeleNotes/4-PSPatterns.js
@@ -287,3 +287,19 @@ function countUniqueValues(arr){
 }
 
 console.log(countUniqueValues([]))
+
+
+// ---------------------
+
+/*
+    SLIDING WINDOW
+
+    This pattern involves creating a window which can either be an array
+    or a number from one position to another
+    
+    Depending on a certain condition, the window either increases or closes (and a new window is created)
+
+    Very useful for keep track of a subset of data in an array/string etc.
+
+
+*/
