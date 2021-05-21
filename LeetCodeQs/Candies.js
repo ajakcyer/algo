7f5f95e24b2f3e0c310a397f4e6f6e42175c5626
ajakcyer <ajakcyer97@gmail.com>
2021-05-21 11:18:26 -0400

    candies algorithm leetcode problem

---
+++ LeetCodeQs/Candies.js
@@ -0,0 +1,5 @@
+/*
+    Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+    
+    For each kid check if there is away to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies
+*/
