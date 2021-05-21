010f642bd93ae72bfdd8feccefed1ec36e0c21f7
ajakcyer <ajakcyer97@gmail.com>
2021-05-21 11:21:12 -0400

    csetting up problem for what I need to return

--- LeetCodeQs/Candies.js
+++ LeetCodeQs/Candies.js
@@ -1,5 +1,13 @@
 /*
     Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
-    
+
     For each kid check if there is away to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies
 */
+
+function kidsWithCandies(candies, extraCandies){
+ 
+    // return an array with true/false values for each kid in the array if they can have the greatest # of candies if given the extraCandies
+
+}
+
+console.log(kidsWithCandies([4,2,1,1,2], 1))
